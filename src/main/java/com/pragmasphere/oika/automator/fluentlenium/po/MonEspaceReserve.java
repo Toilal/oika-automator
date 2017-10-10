b916395b8ce046f858262f788fb71eacc8fbd4b8
Toilal <toilal.dev@gmail.com>
2017-10-10 18:58:08 +0200

    Mise à jour suite à évolution du site

--- src/main/java/com/pragmasphere/oika/automator/fluentlenium/po/MonEspaceReserve.java
+++ src/main/java/com/pragmasphere/oika/automator/fluentlenium/po/MonEspaceReserve.java
@@ -4,7 +4,10 @@ import com.pragmasphere.oika.automator.commands.auth.Auth;
 import org.fluentlenium.core.FluentPage;
 import org.fluentlenium.core.annotation.PageUrl;
 import org.fluentlenium.core.domain.FluentWebElement;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.concurrent.TimeUnit;
 
 @PageUrl("http://www.oikaoika.fr/mon-espace-reserve")
 public class MonEspaceReserve extends FluentPage {
@@ -17,9 +20,15 @@ public class MonEspaceReserve extends FluentPage {
     @FindBy(css = "#espace-reserve input[type='submit']")
     private FluentWebElement submit;
 
+    @FindBy(css = "#qLbar_wrap")
+    private FluentWebElement loader;
+
     public void login(final Auth auth) {
+        await().atMost(15, TimeUnit.SECONDS).until(loader).not().present();
         login.write(auth.getLogin());
         password.write(auth.getPassword());
+        // A Corriger dans fluentlenium ?
+        // await().atMost(15, TimeUnit.SECONDS).until(submit).clickable();
         submit.click();
     }
 }
